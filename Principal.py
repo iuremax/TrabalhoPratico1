15cca42883d21cc9b1727a99aee7ff725b0c8e24
iuremax <71097352+iuremax@users.noreply.github.com>
2020-09-21 15:18:44 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -1,19 +1,28 @@
-from model import GrafoMain as gm
-
-g = {"a": ["d","e"],
-     "b": ["c"],
-     "c": ["b", "c", "d", "e"],
-     "d": ["a", "c"],
-     "e": ["c"],
-     "f": []
-     }
-
-grafo = gm.Grafo(g)
-
-print(grafo)
-print("Vértices isolados:")
-print(grafo.vertices_isolados())
-print("Caminho de a até e:")
-print(grafo.achar_caminho("a","e"))
-print("Caminhos de a até e:")
-print(grafo.achar_todosCaminhos("a","e"))
+import grafinho as g
+grafo = g.Grafo()
+def menu():
+  opc = 0
+  print("Criando um Grafo\n")
+  while opc != 6:
+    opc = int(input("1 - Criar Vértices\n""2 - Criar Arestas\n""3 - Listar Vértices\n""4 - Listar Arestas\n""5 - Imprimir Grafo\n""6 - Sair\n""Selecionar :"))
+    if opc == 1:
+      print()
+      grafo.adicionar_vertice(input("Adicione um vértice: "))
+      print()
+    elif opc == 2:
+      print()
+      grafo.adicionar_aresta(input("Adicione uma aresta: "))
+      print()
+    elif opc == 3:
+      print()
+      print(grafo.vertices())
+      print()
+    elif opc == 4:
+      print()
+      print(grafo.arestas())
+      print()
+    elif opc == 5:
+      print()
+      print(grafo)
+      print()
+menu()
